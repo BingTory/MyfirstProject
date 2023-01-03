7612607b210a0b3dbd618167a0bcc7d9003ed82f
BingTory <bingtory@kakao.com>
2023-01-03 17:16:01 +0900

    Initial Commit

---
+++ MyfirstProject/MyfirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstProjectApp.swift
+//  MyfirstProject
+//
+//  Created by 김광훈 on 2023/01/03.
+//
+
+import SwiftUI
+
+@main
+struct MyfirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
